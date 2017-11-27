ec112ea90718980fe396393bda0c01d5a8ce0396
López Ricardo Ezequiel <mail@lopezezequiel.com>
2017-11-27 11:11:12 -0300

    no comments

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const mongodb = require('mongodb');
 const WebSocket = require('ws');
 const ws = new WebSocket.Server({
     port: process.env.DB_PORT || 3000,
-    ssl_key: 'host.key', ssl_cert: 'host.cert'
+    ssl_key: 'host.key', ssl_cert: 'host.cert',
+    host:'0.0.0.0'
 });
 
 //    port: 3000, ssl: true, ssl_key: 'host.key', ssl_cert: 'host.cert' });
